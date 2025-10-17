f76ef5339f151496a7d3031cc8459a714f823764
pcbproj <pcbproj@yandex.ru>
2025-10-17 15:06:43 +0300

    removed external H-files

--- send_repo_to_github.bat
+++ send_repo_to_github.bat
@@ -1,4 +1,4 @@
-git remote add origin https://github.com/pcbproj/MCU_V3_Lessons_src.git
+git remote add origin https://github.com/pcbproj/USART_Lesson.git
 git branch -M main
 git push -u origin main
 pause
